08cd6fb351acc7d33401e6c4f280ee5954004ff8
PutraAdam08 <putraadam322@gmail.com>
2025-10-07 19:43:22 +0800

    - Add user routes - Add middleware

---
+++ routes/user_routes.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"BSTproject.com/controller"
+	"BSTproject.com/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(router *gin.Engine, userController *controller.UserController, jwtService middleware.JWTService) {
+	userRoutes := router.Group("/users")
+	{
+		userRoutes.POST("/register", userController.Register)
+		userRoutes.POST("/login", userController.Login)
+		userRoutes.GET("", middleware.Authenticate(jwtService), userController.GetUser)
+	}
+}
